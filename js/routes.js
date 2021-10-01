4ba96d46749234fa5a0cefc9e64a745f487b4e2a
Amanda Toni <amanda.toni@zarpsystem.com.br>
2021-10-01 17:09:35 -0300

    Realizando ajustes tcc

---
+++ js/routes.js
@@ -0,0 +1,57 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('page5', {
+    url: '/page5',
+    templateUrl: 'templates/page5.html',
+    controller: 'page5Ctrl'
+  })
+
+  .state('page6', {
+    url: '/signup',
+    templateUrl: 'templates/page6.html',
+    controller: 'page6Ctrl'
+  })
+
+  .state('page7', {
+    url: '/cadastroAnimal',
+    templateUrl: 'templates/page7.html',
+    controller: 'page7Ctrl'
+  })
+
+  .state('page8', {
+    url: '/page8',
+    templateUrl: 'templates/page8.html',
+    controller: 'page8Ctrl'
+  })
+
+  .state('page9', {
+    url: '/cADASTRO3',
+    templateUrl: 'templates/page9.html',
+    controller: 'page9Ctrl'
+  })
+
+  .state('page10', {
+    url: '/page10',
+    templateUrl: 'templates/page10.html',
+    controller: 'page10Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+
+});
